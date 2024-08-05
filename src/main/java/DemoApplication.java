4a9137480a261657e8258dc0f17016852f7fe4e1
José Neto <53952575+josenetoo@users.noreply.github.com>
2024-08-05 15:40:15 -0300

    Update DemoApplication.java

--- src/main/java/DemoApplication.java
+++ src/main/java/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot with GraalVM!! Oracle DevOps Day - OCI Devops";
+        return "Hello from Spring Boot with GraalVM!! Oracle DevOps Day - OCI Devops !! Nilzete!";
     }
 
     public static void main(String[] args) {
